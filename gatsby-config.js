4d805e981ce6685a536a5f39683d606147895dae
itskaustubh <kaustubhpb@gmail.com>
2021-12-01 01:05:39 +0530

    deploy attempt 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
+  pathPrefix: "/creative-studio",
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
